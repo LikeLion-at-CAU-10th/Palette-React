1e52d43e00d1e3852c7596395b739687ceefe452
Yu harin <yuharin0707@naver.com>
2022-08-06 11:59:58 +0900

    네브바 완성

---
+++ src/component/Nav.js
@@ -0,0 +1,95 @@
+import React, {useState} from 'react'
+import { NavLink } from "react-router-dom";
+import styled from 'styled-components';
+import { useNavigate, Outlet } from 'react-router-dom';
+import logo from '../data/logo.png'
+import account from '../data/account.png'
+import NavFolder from './NavFolder';
+
+const Nav = () => {
+    // const navigate = useNavigate();
+
+    const NavBar=styled.div`
+    display: flex;
+    align-items: center;
+    background-color: black;
+    color: white;
+    height: 6.11vh;
+    font-size: 1.25vw;
+    font-family: "Poppins-Bold";
+    padding: 0 0 0 19vw;
+    `
+
+    const NavText=styled.div`
+    text-decoration: none;
+    margin: 2.5vw;
+    cursor: pointer;
+    color: white;
+    position: relative;
+    /* top: 1.3vw; */
+    `
+
+    const LogoImg = styled.img`
+    height: 1.5278vw;
+    width: 3.4722vw;
+    margin: 0 0 0 -1vw;
+    `
+
+    const NavLeft = styled.div`
+    margin: 0 29vw 0 0;
+    `
+    const NavCenter = styled.div`
+    display: flex;
+    margin: 0 0 0 5vw;
+    `
+
+    const AccountImg = styled.img`
+    width: 2.2vw;
+    height: 2.2vw;
+
+    `
+
+    const NavRight=styled.div`
+    margin: 0.5vw 0 0 11vw;
+    `
+
+    const [showModal, setModal] = useState(false);
+
+    const openModal = () =>{
+        setModal(true);
+    }
+
+    const closeModal = () =>{
+        setModal(false);
+    }
+
+
+
+    return (
+        <>
+        <NavBar>
+            <NavLeft>
+                <NavLink to ='/' ><LogoImg src={logo} alt="no imag"/></NavLink> 
+            </NavLeft>
+            <NavCenter>
+                <NavLink to ='/calendar' style={{textDecoration: 'none'}} id='drama'><NavText>calendar</NavText></NavLink>
+                <NavLink to ='/post' style={{textDecoration: 'none'}} ><NavText>post</NavText></NavLink>
+                <NavLink to ='/folder' style={{textDecoration: 'none'}}>
+
+                    <NavText onClick={openModal}>folder</NavText> 
+                    <NavFolder showModal={showModal} closeModal={closeModal} /></NavLink>
+            </NavCenter>
+            <NavRight>
+                <NavLink to='/account'><AccountImg src={account} alt="no imag"/></NavLink>
+            </NavRight>
+        </NavBar>
+
+        <Outlet  />
+        
+        </>
+
+        
+    )
+}
+
+export default Nav
---
+++ src/component/NavFolder.js
@@ -0,0 +1,63 @@
+import React, {useEffect, useState} from 'react'
+import styled from 'styled-components'
+import folderColor from '../data/folderColorData'
+
+const NavFolderBG = styled.div`
+position: fixed;
+background-color: transparent;
+`
+
+const FolderModalCon = styled.div`
+display: flex;
+position: fixed;
+right: 11vw;
+top: 4vw;
+`
+
+const Folders=styled.div`
+background-color: ${(props)=> props.color};
+border-radius: 50%;
+padding: 0.5vw;
+width: 1vw;
+height: 1vw;
+margin: 0.3vw;
+border: 0.02vw solid white;
+`
+
+const CloseButton = styled.div`
+background-color: skyblue;
+width: 7vw;
+height: 3vw;
+position: fixed;
+top: 0.5vw;
+opacity: 0;
+`
+
+
+
+const NavFolder = ({showModal, closeModal}) => {
+
+    return (
+<>
+        {showModal?
+        <>
+        <CloseButton onClick={closeModal}/>
+        <NavFolderBG>
+        <FolderModalCon>{folderColor.folderData.map((folders, i)=>(
+            <Folders color={folders.color} urlPara={folders.urlPara}></Folders>
+        ))}
+            
+        </FolderModalCon>
+        </NavFolderBG></>
+        
+    : null
+    }
+
+    </>
+
+    
+        
+    )
+}
+
+export default NavFolder
