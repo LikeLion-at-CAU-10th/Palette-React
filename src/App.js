c5df995271bd8ffe894eb127a5baee7f28f78f99
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2022-08-14 18:16:34 +0900

    refactor: NavBar 라우터 밖으로 구조 수정

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import Nav from "./component/Nav"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Nav from "./component/Nav";
 
 const App = () => {
-
-return (
+  return (
     <>
-    <Routes>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<></>}></Route>
+        <Route path="/calendar" element={<></>} />
+        <Route path="/post" element={<></>} />
+        <Route path="/folder" element={<></>} />
 
-
-        <Route path="/" element={<Nav/>}>
-            <Route path="/" element={<></>} ></Route>
-            <Route path="/calendar" element={<></>} />
-            <Route path="/post" element={<></>} />
-            <Route path="/folder" element={<></>} />
-
-            <Route path="/post-detail" element={<></>}/>
-            <Route path="/account" element={<></>}/>
-        </Route>
-        
-
-        
-    </Routes>
-    
+        <Route path="/post-detail" element={<></>} />
+        <Route path="/account" element={<></>} />
+      </Routes>
     </>
-    
+  );
+};
 
-
-)
-}
-
-export default App
+export default App;
