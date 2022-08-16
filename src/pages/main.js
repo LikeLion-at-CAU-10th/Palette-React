23c0d917ed078aee9a080e0ee7187b15dd934beb
Haeyoon-Cho <yoonn_ee@naver.com>
2022-08-17 00:28:23 +0900

    수정

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,15 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled, { createGlobalStyle } from "styled-components";
 
 const Circles = styled.div`
   cursor: pointer;
-  position: absolute;
   width: 8.33vw;
   height: 8.33vw;
-  top: 47.5vh;
   border-radius: 100%;
-  background: ${(props) => props.backGround};
-  left: ${(props) => props.left};
+  background-color: ${(props) => props.bgColorHex};
+  visibility : ${(props) => (props.visible ? "visible" : "hidden")};
 `;
 
 const GlobalStyle = createGlobalStyle`
@@ -18,16 +16,20 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const Tabs = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 20%;
+  width: 100%;
+`;
+
 const MenuTab = styled.div`
   cursor: pointer;
   font-weight: 700;
-  font-size: 1.25vw;
-  line-height: 1.88vw;
+  font-size: 1.3vw;
+  line-height: 5vw;
   color: #ffffff;
   white-space: nowrap;
-  position: absolute;
-  height: 1.88vw;
-  top: 5.35vh;
   text-decoration-line: underline;
   text-decoration-thickness: 2px;
   left: ${(props) => props.left};
@@ -35,74 +37,141 @@ const MenuTab = styled.div`
 `;
 
 const Buttons = styled.div`
-  position: absolute;
   width: 1.88vw;
   height: 1.88vw;
-  left: ${(props) => props.left};
-  top: ${(props) => props.top};
-  background: ${(props) => props.backGround};
+  background-color: ${(props) => props.bgColorHex};
   border-radius: 100%;
-  transition: 0.1s;
-  //visibility: hidden;
   cursor: pointer;
 `;
 
+const CirclesDom = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80%;
+`;
+
+const MainPageDom = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const EditButtonDom = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+// const Save = styled.div`
+//   width: 4.3vw;
+//   height: 1.8vw;
+//   background-color: #000000;
+//   border: 2px solid #ffffff;
+//   border-radius: 2em 2em 2em 2em;
+//   color: #ffffff;
+//   text-align: center;
+//   cursor: pointer;
+// `;
+
+const PalettesDom = styled.div`
+  display: flex;
+  width: 100%;
+  visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+`;
+
+const selectColor = (color) => {
+  switch (color) {
+    case "red":
+      return "#ff8b8be6";
+    case "orange":
+      return "#ffad60";
+    case "yellow":
+      return "rgba(250, 232, 76, 0.9)";
+    case "green":
+      return "#83e686e6";
+    case "blue":
+      return "hsl(227, 100%, 64%)";
+    case "purple":
+      return "purple";
+    case "black":
+      return "black";
+    default:
+      return "black";
+  }
+};
+
 const Main = () => {
+  const paletteColors = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple",
+    "black",
+  ];
+  const [visibleColor, setVisibleColor] = useState([
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+  ]);
+  const [showEdit, setShowEdit] = useState(false);
+
+  const changeEditBtn = () => {
+    setShowEdit(!showEdit);
+  };
+
+  const changeVisible = (i) => {
+    const copy = [...visibleColor];
+    copy[i] = !copy[i];
+    setVisibleColor(copy);
+  };
+
+  // const closeColor = () => {
+  //   setColor(false);
+  // };
+
   return (
-    <>
+    <MainPageDom>
       <GlobalStyle />
-      <MenuTab left="36.40vw" width="5.83vw">
-        calendar
-      </MenuTab>
-      <MenuTab left="48.54vw" width="2.92vw">
-        post
-      </MenuTab>
-      <MenuTab left="57.76vw" width="6.04vw">
-        edit
-      </MenuTab>
-      <Buttons
-        left="57.42vw"
-        top="13.52vh"
-        backGround="rgba(205, 98, 255, 0.9)"
-      ></Buttons>
-      <Buttons
-        left="58.60vw"
-        top="9.744vh"
-        backGround=" rgba(255, 58, 58, 0.9)"
-      ></Buttons>
-      <Buttons
-        left="61.10vw"
-        top="9.744vh"
-        backGround="rgba(255, 141, 35, 0.9)"
-      ></Buttons>
-      <Buttons
-        left="62.28vw"
-        top="13.68vh"
-        backGround="rgba(247, 226, 37, 0.9)"
-      ></Buttons>
-      <Buttons
-        left="61.10vw"
-        top="17.3vh"
-        backGround="rgba(85, 214, 90, 0.9)"
-      ></Buttons>
-      <Buttons
-        left="58.60vw"
-        top="17.3vh"
-        backGround="rgba(0, 56, 255, 0.9)"
-      ></Buttons>
-      <Buttons
-        left="59.85vw"
-        top="13.52vh"
-        backGround="rgba(101, 101, 101, 0.9)"
-      ></Buttons>
-      <Circles backGround="rgba(205, 98, 255, 0.9)" left="64.86vw "></Circles> 
-      <Circles backGround="rgba(255, 58, 58, 0.9)" left="58.54vw "></Circles> 
-      <Circles backGround="rgba(255, 141, 35, 0.9)" left="52.22vw "></Circles>
-      <Circles backGround="rgba(247, 226, 37, 0.9)" left="45.9vw "></Circles> 
-      <Circles backGround="rgba(85, 214, 90, 0.9)" left="39.44vw "></Circles> 
-      <Circles backGround="rgba(0, 56, 255, 0.9)" left="33.13vw "></Circles> 
-      <Circles backGround="rgba(101, 101, 101, 0.9)" left="26.81vw "></Circles>
-    </>
+      <Tabs>
+        <MenuTab left="36.40vw" width="13vw">
+          calendar
+        </MenuTab>
+        <MenuTab left="48.54vw" width="10vw">
+          post
+        </MenuTab>
+        <EditButtonDom>
+          <MenuTab onClick={changeEditBtn} left="57.76vw" width="8vw">
+            edit
+          </MenuTab>
+          <PalettesDom visible={showEdit}>
+            {visibleColor.map((visible, i) => (
+              <Buttons
+                key={i}
+                onClick={() => {
+                  changeVisible(i);
+                }}
+                bgColorHex={selectColor(paletteColors[i])}
+              />
+            ))}
+          </PalettesDom>
+        </EditButtonDom>
+      </Tabs>
+
+      <CirclesDom>
+        {visibleColor.map((visible, i) => (
+          <Circles
+            key={i}
+            bgColorHex={selectColor(paletteColors[i])}
+            visible={visible}
+          />
+        ))}
+      </CirclesDom>
+    </MainPageDom>
   );
 };
 
