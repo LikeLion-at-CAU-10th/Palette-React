e38fa576f1609d724619d7a3575640bf651073d6
Haeyoon-Cho <yoonn_ee@naver.com>
2022-08-16 16:29:41 +0900

    folder-final

--- src/pages/Folder.js
+++ src/pages/Folder.js
@@ -20,6 +20,9 @@ const Folders = styled.div`
 `;
 
 const FolderIndexBar = styled.div`
+  position: absolute;
+  top: 8.1vh;
+  right: 19.8vw;
   height: 6vh;
   width: 22.2vh;
   background: #f7e125;
@@ -28,13 +31,16 @@ const FolderIndexBar = styled.div`
   font-weight: 900;
   transform: rotate(-90deg);
   text-align: center;
+  font-family: "Poppins-Black";
 `;
 
 const FolderIndex = styled.div`
+  position: relative;
   height: 76vh;
   background: #f7e125;
   width: 20vw;
   padding: 2vw;
+  z-index: -1;
 `;
 
 const Pages = styled.div`
@@ -59,24 +65,25 @@ const FolderPage = () => {
 
       <Folders>
         <Wrapper>
-          <FolderIndexBar>yellow</FolderIndexBar>
-          <FolderIndex></FolderIndex>
+          <FolderIndex>
+            <FolderIndexBar> yellow</FolderIndexBar>
+          </FolderIndex>
+          {data.postListData.map((data) => (
+            <Pages key={data.i} bgColor={data.color}>
+              <text>
+                <p>
+                  <h4>{data.date}</h4>
+                </p>
+                <p>
+                  <h5>{data.title}</h5>
+                </p>
+                <p>
+                  <h4>{data.content}</h4>
+                </p>
+              </text>
+            </Pages>
+          ))}
         </Wrapper>
-        {data.postListData.map((data) => (
-          <Pages key={data.i} bgColor={data.color}>
-            <text>
-              <p>
-                <h4>{data.date}</h4>
-              </p>
-              <p>
-                <h5>{data.title}</h5>
-              </p>
-              <p>
-                <h4>{data.content}</h4>
-              </p>
-            </text>
-          </Pages>
-        ))}
       </Folders>
     </div>
   );
