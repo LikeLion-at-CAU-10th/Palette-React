1952ca3bd7b3fee628ea10bf1a77a119cd027361
Haeyoon-Cho <yoonn_ee@naver.com>
2022-08-17 04:25:53 +0900

    change main >Main

--- src/App.js
+++ src/App.js
@@ -8,19 +8,55 @@ import PostDetail from "./pages/PostDetail";
 const App = () => {
   return (
     <>
-    <Routes>
+      <Routes>
         <Route path="/" element={<Main />}></Route>
-        <Route path="/calendar" element={<><Nav /></>} />
-        <Route path="/post" element={<><Nav/><Post /></>} />
-        <Route path="/folder" element={<Navigate to="/folder/purple"/>}/>
-        <Route path="/folder/:category" element={<><Nav /><div>안녕</div></>} />
-        <Route path="/post-detail/:category" element={<><Nav /><PostDetail/></>} />
-        <Route path="/account" element={<><Nav/></>} />
-    </Routes>
+        <Route
+          path="/calendar"
+          element={
+            <>
+              <Nav />
+            </>
+          }
+        />
+        <Route
+          path="/post"
+          element={
+            <>
+              <Nav />
+              <Post />
+            </>
+          }
+        />
+        <Route path="/folder" element={<Navigate to="/folder/purple" />} />
+        <Route
+          path="/folder/:category"
+          element={
+            <>
+              <Nav />
+              <div>안녕</div>
+            </>
+          }
+        />
+        <Route
+          path="/post-detail/:category"
+          element={
+            <>
+              <Nav />
+              <PostDetail />
+            </>
+          }
+        />
+        <Route
+          path="/account"
+          element={
+            <>
+              <Nav />
+            </>
+          }
+        />
+      </Routes>
     </>
   );
 };
 
 export default App;
-
-
