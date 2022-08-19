97bb38ea2f277dd1f952141bc18bc05ece2ba0ff
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2022-08-19 22:03:52 +0900

    feat: 회원가입 폼 비밀번호/이메일 형식 정규식 추가 및 비밀번호  확인

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { GlobalStyle, SignUpBtn, MainPageDom } from "../login";
 import styled from "styled-components";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = styled.div`
   width: 490px;
-  height: 300px;
+  height: 295px;
   padding: 3% 3% 3% 3%;
   background-color: rgba(69, 69, 69, 1);
   border-radius: 20px;
@@ -48,6 +50,7 @@ const FormInput = styled.input`
 const InputDom = styled.div`
   display: flex;
   width: 500px;
+  margin-bottom: 5%;
   justify-content: flex-start;
   font-family: "Poppins-Bold";
 `;
@@ -62,7 +65,95 @@ const InputTitle = styled.div`
   font-weight: 400;
 `;
 
+const Mismatch = styled.div`
+  width: 100%;
+  padding-left: 52%;
+  margin-top: -3%;
+  color: white;
+  display: flex;
+  align-items: center;
+  font-size: 0.9em;
+  visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+`;
 const SignUpPage = () => {
+  const [nickname, setNickname] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [mismatchError, setMismatchError] = useState(false);
+  const [signUpError, setSignUpError] = useState("");
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
+  const [email, setEmail] = useState("");
+  const emailRegex =
+    /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
+  const passwordRegEx = /^[A-Za-z0-9]{8,30}$/;
+  const navigate = useNavigate();
+
+  const onChangeName = (e) => {
+    setNickname(e.target.value);
+  };
+
+  const onChangePassword = useCallback(
+    (e) => {
+      setPassword(e.target.value);
+      setMismatchError(e.target.value !== passwordCheck);
+    },
+    [passwordCheck]
+  );
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setMismatchError(e.target.value !== password);
+    },
+    [password]
+  );
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onSubmit = useCallback(
+    (e) => {
+      console.log(email);
+      console.log(nickname);
+      console.log(password);
+      console.log(passwordCheck);
+      if (!mismatchError) {
+        // mismatchError이 false일 때, 즉, match 되었을 때
+        if (!emailRegex.test(email)) {
+          alert("이메일 형식이 틀렸어");
+        } else if (password.match(passwordRegEx) === null) {
+          alert("비번 형식이 틀렸어");
+        } else {
+          setSignUpError("");
+          setSignUpSuccess(false);
+          axios
+            .post(
+              "https://localhost:8000/signup",
+              {
+                nickname,
+                password,
+                email,
+              },
+              {
+                "Content-Type": "application/json",
+              }
+            )
+            .then((res) => {
+              setSignUpSuccess(true);
+              if (res.data.success) {
+                navigate("/login");
+              }
+            })
+            .catch((error) => {
+              setSignUpError(error.response.data);
+            });
+        }
+      }
+    },
+    [email, nickname, password, passwordCheck, mismatchError]
+  );
+
   return (
     <MainPageDom>
       <GlobalStyle />
@@ -71,29 +162,50 @@ const SignUpPage = () => {
         <InputDom>
           <InputTitle>닉네임</InputTitle>
           <FormInputDom>
-            <FormInput placeholder="사용할 닉네임을 입력해주세요."></FormInput>
-          </FormInputDom>
-        </InputDom>
-        <InputDom>
-          <InputTitle>비밀번호</InputTitle>
-          <FormInputDom>
-            <FormInput placeholder="8~30자 이내 비밀번호를 입력해주세요."></FormInput>
-          </FormInputDom>
-        </InputDom>
-        <InputDom>
-          <InputTitle>비밀번호 확인</InputTitle>
-          <FormInputDom>
-            <FormInput placeholder="비밀번호 확인"></FormInput>
+            <FormInput
+              onChange={onChangeName}
+              type="text"
+              placeholder="사용할 닉네임을 입력해주세요."
+              value={nickname}
+            ></FormInput>
           </FormInputDom>
         </InputDom>
         <InputDom>
           <InputTitle>이메일</InputTitle>
           <FormInputDom>
-            <FormInput placeholder="이메일 주소를 입력해주세요."></FormInput>
+            <FormInput
+              onChange={onChangeEmail}
+              type="text"
+              placeholder="이메일 주소를 입력해주세요."
+              value={email}
+            ></FormInput>
           </FormInputDom>
         </InputDom>
+        <InputDom>
+          <InputTitle>비밀번호</InputTitle>
+          <FormInputDom>
+            <FormInput
+              onChange={onChangePassword}
+              type="text"
+              placeholder="8~30자 이내 비밀번호를 입력해주세요."
+              value={password}
+            ></FormInput>
+          </FormInputDom>
+        </InputDom>
+        <InputDom>
+          <InputTitle>비밀번호 확인</InputTitle>
+          <FormInputDom>
+            <FormInput
+              onChange={onChangePasswordCheck}
+              type="text"
+              placeholder="비밀번호 확인"
+              value={passwordCheck}
+            ></FormInput>
+          </FormInputDom>
+        </InputDom>
+        <Mismatch visible={mismatchError}>비밀번호가 다릅니다.</Mismatch>
       </LoginForm>
-      <SignUpBtn>SIGN UP</SignUpBtn>
+      <SignUpBtn onClick={onSubmit}>SIGN UP</SignUpBtn>
     </MainPageDom>
   );
 };
